ba79d80536546f7d95486ff0645a30d34d2a429e
Michael Doliner <msd83@cornell.edu>
2015-03-23 16:38:45 -0400

    Adds Graph BFS.

---
+++ Graphs/breadth_first_search.js
@@ -0,0 +1,41 @@
+/* A breadth-first search using the graph implementation in graph_adj_list.js
+   This is just the most basic implementation, it just finds a vertex. 
+   Your implementation will depend on the types of problems you're trying
+   to solve. Applications for topological sort and checking for bipartate
+   are also located in this folder. */
+
+Graph.prototype.breadthFirstSearch = function (target) {
+  var status = [], result, graph = this;
+  for (var i = 0; i < this.nVertices; i++) {
+    status[i] = "U"; //U = Undiscovered, D = Discovered, P = Processed
+  }
+  for (var i = 0; i < this.nVertices; i++) {
+    if (status[i] === "U") {
+      //Will do a breadth-first search on every component until it finds the target
+      result = result || bfs(i, target);
+    }
+  }
+
+  return result;
+
+  function bfs (startVtx, target) {
+    var queue = [startVtx];
+    while (queue.length > 0) {
+      var vertex = queue.shift();
+      if (vertex === target) {
+        return target;
+      }
+      var adjList = graph.adjLists[vertex];
+      while (adjList) {
+        var adjVtx = adjList.vtx;
+        if (status[adjVtx] === "U") {
+          queue.push(adjVtx);
+          status[adjVtx] = "D";
+        }
+        adjList = adjList.next;
+      }
+      status[vertex] = "P";
+    }
+  };
+
+};
